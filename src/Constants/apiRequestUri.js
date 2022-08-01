c66f9911e80c8162bc0617f319a27f462683b828
Himanshu Mahara <himanshu.mahara@publicissapient.com>
2022-08-01 12:17:56 +0530

    test

--- src/Constants/apiRequestUri.js
+++ src/Constants/apiRequestUri.js
@@ -6,6 +6,6 @@ if(mock){
     API_END_POINT = "http://localhost:4000";
 }
 
-export const VERIFY_EMAIL_API_URL= API_END_POINT + "/login";
+export const VERIFY_EMAIL_API_URL= API_END_POINT + "/login"; 
 
 
